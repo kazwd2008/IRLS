419b795d3e7d34cec81a5d7407fbca9d6cac1296
wada, kazumi <kazwd2008@gmail.com>
2018-10-27 01:35:42 +0900

    Minor change
    Added some comments

--- Hirls.r
+++ Hirls.r
@@ -8,14 +8,23 @@
 #       Ver. 3.0  2012/12/18  平均絶対偏差版と中央絶対偏差版作成    #
 #                             Hirls.ave       Hirls.med             #
 #       Ver. 3.1  2012/12/19  収束条件パラメータ追加                #
-#       Ver. 3.2  2013/04/25  切片のない回帰用のHirls.avexと        #
-#                               Hirls.madxを作成                    #
+#       Ver. 3.2  2013/04/25  切片のない回帰版を作成        #
+#                             Tirls.avex     Tirls.medxを作成  					#
 #       Ver. 3.3  2014/08/05  WLSの場合残差平均0ではないので        #
-#                               AADの計算を修正  					#
+#                               AADの計算を修正 <= 間違い 09/05		#
+#       Ver. 3.4  2014/08/25  初期値計算はOLS                       #
+#       Ver. 4.0  2014/09/05  残差から平均・中央値は引かない        #
+#       Ver. 4.1  2018/10/26  公開用にコメント整備
 #####################################################################
-#  関数Hirls パラメータ
-#   y1      単回帰の目的変数（必須）
-#   x1      単回帰の説明変数（必須）
+#  収録:  Huberウェイトを使用したIRLSアルゴリズムの関数
+#    Hirls.ave     尺度AAD, 回帰モデル用
+#    Hirls.med     尺度MAD, 回帰モデル用
+#    Hirls.avex    尺度AAD, 切片のない回帰モデル
+#    Hirls.medx   尺度MAD,  切片のない回帰モデル
+#####################################################################
+#  関数パラメータ
+#   y1      目的変数（必須）
+#   x1      説明変数（必須）
 #   rt      乗率。指定がない場合デフォルトは1
 #   c1      ウエイト関数用コンスタント。デフォルトは1.686(正規分布想定)。
 #       1.44でTukeyのbiweight c=4、 2.16でc=6、2.88でTukeyのc=8相当。
@@ -28,19 +37,18 @@
 #   rp      ループ回数
 #   s1      平均絶対残差(AAD)または中央絶対偏差(MAD)の変遷データ
 ###################################################################
-#	Hirls.avexとHirls.madxは、切片のない回帰を行う。
-###################################################################
 
 Hirls.ave <- function(y1, x1, rt=rep(1, length(y1)), c1=1.15, dat="", rp.max=150, cg.rt=0.01) {
 
   if (dat!="") attach(get(dat))         # データフレーム指定があるときのみ 
   s1.cg <- rep(0, rp.max)               # 2011.03.07 MADの変遷を保存
-  R0 <- lm(y1~x1, weights=rt)						            # 初期値はウエイトをつけないOLS
+  R0 <- lm(y1~x1, weights=rt) 
+  #R0 <- lm(y1~x1)       				# 初期値は普通のOLS
   
   Hb2 <- R0
   rp1 <- 1                              # ループ回数
   s0 <- 0			# 最低１回はループするように差が0.01より大きくなるよう値をセット
-  s1 <- s1.cg[rp1] <- mean(abs(Hb2$residuals-mean(Hb2$residuals)))        # 平均絶対残差
+  s1 <- s1.cg[rp1] <- mean(abs(Hb2$residuals))        # 平均絶対残差
 
   #### ウエイト算出
     w1 <- s1*c1 / abs(Hb2$residuals)
@@ -54,7 +62,7 @@ Hirls.ave <- function(y1, x1, rt=rep(1, length(y1)), c1=1.15, dat="", rp.max=150
       s0 <- s1	
       Hb2 <- lm(y1~x1, weights=w1*rt) 
       rp1 <- rp1 + 1			# ループカウンタ
-      s1 <- s1.cg[rp1] <- mean(abs(Hb2$residuals-mean(Hb2$residuals)))	# 平均絶対残差
+      s1 <- s1.cg[rp1] <- mean(abs(Hb2$residuals))	# 平均絶対残差
       w1 <- s1*c1 / abs(Hb2$residuals)
       w1[which(abs(Hb2$residuals) <= s1*c1)] <- 1
     }
@@ -66,7 +74,8 @@ Hirls.med <- function(y1, x1, rt=rep(1, length(y1)), c1=1.44, dat="", rp.max=150
 
   if (dat!="") attach(get(dat))         # データフレーム指定があるときのみ 
   s1.cg <- rep(0, rp.max)               # 2011.03.07 MADの変遷を保存
-  R0 <- lm(y1~x1, weights=rt)						            # 初期値はウエイトをつけないOLS
+  R0 <- lm(y1~x1, weights=rt) 
+  #R0 <- lm(y1~x1)       				# 初期値は普通のOLS
   
   Hb2 <- R0
   rp1 <- 1                              # ループ回数
@@ -97,12 +106,13 @@ Hirls.avex <- function(y1, x1, rt=rep(1, length(y1)), c1=1.15, dat="", rp.max=15
 
   if (dat!="") attach(get(dat))         # データフレーム指定があるときのみ 
   s1.cg <- rep(0, rp.max)               # 2011.03.07 MADの変遷を保存
-  R0 <- lm(y1~x1-1, weights=rt)						            # 初期値はウエイトをつけないOLS
+  R0 <- lm(y1~x1-1, weights=rt)  
+  #R0 <- lm(y1~x1-1)       				# 初期値は普通のOLS
   
   Hb2 <- R0
   rp1 <- 1                              # ループ回数
   s0 <- 0			# 最低１回はループするように差が0.01より大きくなるよう値をセット
-  s1 <- s1.cg[rp1] <- mean(abs(Hb2$residuals-mean(Hb2$residuals)))        # 平均絶対残差
+  s1 <- s1.cg[rp1] <- mean(abs(Hb2$residuals))        # 平均絶対残差
 
   #### ウエイト算出
     w1 <- s1*c1 / abs(Hb2$residuals)
@@ -116,7 +126,7 @@ Hirls.avex <- function(y1, x1, rt=rep(1, length(y1)), c1=1.15, dat="", rp.max=15
       s0 <- s1	
       Hb2 <- lm(y1~x1-1, weights=w1*rt) 
       rp1 <- rp1 + 1			# ループカウンタ
-      s1 <- s1.cg[rp1] <- mean(abs(Hb2$residuals-mean(Hb2$residuals)))	# 平均絶対残差
+      s1 <- s1.cg[rp1] <- mean(abs(Hb2$residuals))	# 平均絶対残差
       w1 <- s1*c1 / abs(Hb2$residuals)
       w1[which(abs(Hb2$residuals) <= s1*c1)] <- 1
     }
@@ -128,7 +138,8 @@ Hirls.medx <- function(y1, x1, rt=rep(1, length(y1)), c1=1.44, dat="", rp.max=15
 
   if (dat!="") attach(get(dat))         # データフレーム指定があるときのみ 
   s1.cg <- rep(0, rp.max)               # 2011.03.07 MADの変遷を保存
-  R0 <- lm(y1~x1-1, weights=rt)						            # 初期値はウエイトをつけないOLS
+  R0 <- lm(y1~x1-1, weights=rt)  
+  #R0 <- lm(y1~x1-1)       				# 初期値は普通のOLS
   
   Hb2 <- R0
   rp1 <- 1                              # ループ回数
--- Tirls.r
+++ Tirls.r
@@ -8,41 +8,50 @@
 #       Ver. 3.0  2012/12/18  平均絶対偏差版と中央絶対偏差版作成    #
 #                             Tirls.ave       Tirls.med             #
 #       Ver. 3.1  2012/12/19  収束条件パラメータ追加                #
-#       Ver. 3.2  2013/04/25  切片のない回帰用のTirls.avexと        #
-#                               Tirls.madxを作成  					#
+#       Ver. 3.2  2013/04/25  切片のない回帰版を作成
+#                             Tirls.avex     Tirls.medxを作成  					#
 #       Ver. 3.3  2014/08/05  WLSの場合残差平均0ではないので        #
-#                               AADの計算を修正  					#
+#                               AADの計算を修正 <= 間違い 09/05		#
+#       Ver. 3.4  2014/08/25  初期値計算はOLS                       #
+#       Ver. 4.0  2014/09/05  残差から平均・中央値を引かない        #
+#       Ver. 4.1  2018/10/26  公開用にコメント整備
 #####################################################################
-#  関数Tirls パラメータ
-#   y1      単回帰の目的変数（必須）                               
-#   x1      単回帰の説明変数（必須）
+#  収録:  Tukeyのbiweightを使用したIRLSアルゴリズムの関数
+#    Tirls.ave     尺度AAD, 回帰モデル用
+#    Tirls.med     尺度MAD, 回帰モデル用
+#    Tirls.avex    尺度AAD, 切片のない回帰モデル
+#    Tirls.medx   尺度MAD,  切片のない回帰モデル
+#    RTirls.ave    尺度AAD, 比率モデル
+#####################################################################
+#  関数パラメータ
+#   y1      目的変数（必須）                               
+#   x1      説明変数（必須）
 #   rt      乗率。指定がない場合デフォルトは1
 #   c1      biweight関数用コンスタント。デフォルトはgeneralにパフォーマ
 #            ンスが良い最大値(aveで8)。 
 #   c=4     とてもロバスト 
 #   c=8     ちょっとロバスト
 #   dat     x1, y1が含まれるデータフレーム。指定がなければ使わない
-#   rp.max  ループ回数の上限。特に指定しなければ50回  
+#   rp.max  ループ回数の上限。特に指定しなければ150回  
 ###################################################################
-#  関数Tirls 戻り値
+#  関数戻り値
 #   TK      最終結果
 #   wt      ウエイト
 #   rp      ループ回数
 #   s1      平均絶対残差(AAD)または中央絶対偏差(MAD)の変遷データ
-###################################################################
-#	Tirls.avexとTirls.madxは、切片のない回帰を行う。
 ###################################################################
 
 Tirls.ave <- function(y1, x1, rt=rep(1, length(y1)), c1=8, dat="", rp.max=150, cg.rt=0.01) {
 
   if (dat!="") attach(get(dat))	        # データフレーム指定があるときのみ
   s1.cg <- rep(0, rp.max)               # 2012.12.18 s1の変遷を保存
-  R0 <- lm(y1~x1, weights=rt)           # 初期値は普通のOLS
+  R0 <- lm(y1~x1, weights=rt) 
+  #R0 <- lm(y1~x1)       				# 初期値は普通のOLS
   
   Tk2 <- R0
   rp1 <- 1								# ループ回数
   s0 <- 0			# 最低１回はループするように差が0.01より大きくなるよう値をセット
-  s1 <- s1.cg[rp1] <- mean(abs(Tk2$residuals-mean(Tk2$residuals)))       # 平均絶対残差(MAD)
+  s1 <- s1.cg[rp1] <- mean(abs(Tk2$residuals))       # 平均絶対残差(MAD)
 
   #### ウエイト算出
   	u1 <-Tk2$residuals/(c1*s1)
@@ -57,7 +66,7 @@ Tirls.ave <- function(y1, x1, rt=rep(1, length(y1)), c1=8, dat="", rp.max=150, c
         s0 <- s1	
         Tk2 <- lm(y1~x1, weights=w1*rt) 
         rp1 <- rp1 + 1			# ループカウンタ
-        s1 <- s1.cg[rp1] <- mean(abs(Tk2$residuals-mean(Tk2$residuals)))	# 平均絶対残差
+        s1 <- s1.cg[rp1] <- mean(abs(Tk2$residuals))	# 平均絶対残差
         u1 <-Tk2$residuals/(c1*s1)
         w1 <- (1-u1**2)**2
         w1[which(abs(u1)>=1)] <- 0
@@ -70,12 +79,13 @@ Tirls.med <- function(y1, x1, rt=rep(1, length(y1)), c1=10.03, dat="", rp.max=15
 
   if (dat!="") attach(get(dat))	        # データフレーム指定があるときのみ
   s1.cg <- rep(0, rp.max)               # 2011.03.07 MADの変遷を保存
-  R0 <- lm(y1~x1, weights=rt)           # 初期値は普通のOLS
+  R0 <- lm(y1~x1, weights=rt) 
+  #R0 <- lm(y1~x1)       				# 初期値は普通のOLS
   
   Tk2 <- R0
   rp1 <- 1								# ループ回数
   s0 <- 0			# 最低１回はループするように差が0.01より大きくなるよう値をセット
-  s1 <- s1.cg[rp1] <- mad(Tk2$residuals) # 中央絶対残差(MAD) σ準拠に補正済み
+  s1 <- s1.cg[rp1] <- mad(Tk2$residuals) 			# 中央絶対残差(MAD) σ準拠に補正済み
 
   #### ウエイト算出
   	u1 <-Tk2$residuals/(c1*s1)
@@ -103,12 +113,13 @@ Tirls.avex <- function(y1, x1, rt=rep(1, length(y1)), c1=8, dat="", rp.max=150, 
 
   if (dat!="") attach(get(dat))	        # データフレーム指定があるときのみ
   s1.cg <- rep(0, rp.max)               # 2012.12.18 s1の変遷を保存
-  R0 <- lm(y1~x1-1, weights=rt)           # 初期値は普通のOLS
+  R0 <- lm(y1~x1-1, weights=rt)  
+  #R0 <- lm(y1~x1-1)       				# 初期値は普通のOLS
   
   Tk2 <- R0
   rp1 <- 1								# ループ回数
   s0 <- 0			# 最低１回はループするように差が0.01より大きくなるよう値をセット
-  s1 <- s1.cg[rp1] <- mean(abs(Tk2$residuals-mean(Tk2$residuals)))   # 平均絶対残差(AAD)
+  s1 <- s1.cg[rp1] <- mean(abs(Tk2$residuals))   # 平均絶対残差(AAD)
 
   #### ウエイト算出
   	u1 <-Tk2$residuals/(c1*s1)
@@ -123,7 +134,7 @@ Tirls.avex <- function(y1, x1, rt=rep(1, length(y1)), c1=8, dat="", rp.max=150, 
         s0 <- s1	
         Tk2 <- lm(y1~x1-1, weights=w1*rt) 
         rp1 <- rp1 + 1			# ループカウンタ
-        s1 <- s1.cg[rp1] <- mean(abs(Tk2$residuals-mean(Tk2$residuals)))	# 平均絶対残差
+        s1 <- s1.cg[rp1] <- mean(abs(Tk2$residuals))	# 平均絶対残差
         u1 <-Tk2$residuals/(c1*s1)
         w1 <- (1-u1**2)**2
         w1[which(abs(u1)>=1)] <- 0
@@ -136,7 +147,8 @@ Tirls.medx <- function(y1, x1, rt=rep(1, length(y1)), c1=10.3, dat="", rp.max=15
 
   if (dat!="") attach(get(dat))	        # データフレーム指定があるときのみ
   s1.cg <- rep(0, rp.max)               # 2011.03.07 MADの変遷を保存
-  R0 <- lm(y1~x1-1, weights=rt)           # 初期値は普通のOLS
+  R0 <- lm(y1~x1-1, weights=rt)  
+  #R0 <- lm(y1~x1-1)       				# 初期値は普通のOLS
   
   Tk2 <- R0
   rp1 <- 1								# ループ回数
@@ -156,7 +168,7 @@ Tirls.medx <- function(y1, x1, rt=rep(1, length(y1)), c1=10.3, dat="", rp.max=15
         s0 <- s1	
         Tk2 <- lm(y1~x1-1, weights=w1*rt) 
         rp1 <- rp1 + 1			# ループカウンタ
-        s1 <- s1.cg[rp1] <- mad(Tk2$residuals)	# 中央絶対残差(MAD)
+        s1 <- s1.cg[rp1] <- mad(Tk2$residuals)		# 中央絶対残差(MAD)
         u1 <-Tk2$residuals/(c1*s1)
         w1 <- (1-u1**2)**2
         w1[which(abs(u1)>=1)] <- 0
@@ -165,3 +177,37 @@ Tirls.medx <- function(y1, x1, rt=rep(1, length(y1)), c1=10.3, dat="", rp.max=15
   }
 
 #-----------------------------------------------------------------------------
+RTirls.ave <- function(y1, rt=rep(1, length(y1)), c1=8, dat="", rp.max=150, cg.rt=0.01) {
+
+  if (dat!="") attach(get(dat))	        # データフレーム指定があるときのみ
+  s1.cg <- rep(0, rp.max)               # 2012.12.18 s1の変遷を保存
+  R0 <- lm(y1~1, weights=rt)  
+  #R0 <- lm(y1~1)       				# 初期値は普通のOLS
+  
+  Tk2 <- R0
+  rp1 <- 1								# ループ回数
+  s0 <- 0			# 最低１回はループするように差が0.01より大きくなるよう値をセット
+  s1 <- s1.cg[rp1] <- mean(abs(Tk2$residuals))   # 平均絶対残差(AAD)
+
+  #### ウエイト算出
+  	u1 <-Tk2$residuals/(c1*s1)
+  	w1 <- (1-u1**2)**2
+  	w1[which(abs(u1)>=1)] <- 0
+
+  #### 繰返し計算
+  for (i in 2:rp.max) {                             # 2011.03.07
+      # while (abs(1-s1/s0) >= 0.01) {
+      if (abs(1-s1/s0) < cg.rt) break                # 2012.12.19
+        Tk1 <-Tk2
+        s0 <- s1	
+        Tk2 <- lm(y1~1, weights=w1*rt) 
+        rp1 <- rp1 + 1			# ループカウンタ
+        s1 <- s1.cg[rp1] <- mean(abs(Tk2$residuals))	# 平均絶対残差
+        u1 <-Tk2$residuals/(c1*s1)
+        w1 <- (1-u1**2)**2
+        w1[which(abs(u1)>=1)] <- 0
+    }
+	return(list(TK=Tk2, wt=w1, rp=rp1, s1=s1.cg))
+  }
+#-----------------------------------------------------------------------------
+
